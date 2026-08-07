36d830b3a9e0bb989adebdd7c4f00fff78fb24f2
shubha-wq <shubhasingh1907@gmail.com>
2026-08-07 11:08:59 +0530

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "framerusercontent.com",
-      },
-    ],
-  },
+  output: "export",
 };
 
 export default nextConfig;
